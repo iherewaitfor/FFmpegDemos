bced40a56914d45877f74c4737be47b38cd2dc12
yangkemin <iherewaitfor@163.com>
2023-05-12 21:13:53 +0800

    add decodedemo. 初始化项目。

---
+++ decodedemo/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <Windows.h>
+
+int main(LPWSTR lpCmdLine)
+{
+    int nArgs = 0;
+    LPWSTR* szArglist = NULL;
+    szArglist = CommandLineToArgvW(GetCommandLineW(), &nArgs);
+    std::wcout << L"Hello World" << std::endl;
+    std::wcout << L"the argc " << nArgs;
+    const wchar_t* argv_0 = szArglist[0];
+    std::wcout << L"the argv[0] " << argv_0 << std::endl;
+    if (nArgs > 2) {
+        const wchar_t* argv_1 = szArglist[1];
+        std::wcout << L"the argv[1] " << argv_1 <<std::endl;
+        const wchar_t* argv_2 = szArglist[2];
+        std::wcout << L"the argv[2] " << argv_2;
+    }
+    return 0;
+}
